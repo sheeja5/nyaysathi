4466b6b3a1714583d6a641d1a97e54dfcd7f2612
sheeja5 <sheeja.stc@gmail.com>
2024-12-18 20:07:30 +0530

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -1,2 +1,9 @@
 import streamlit as st
 import google.generativeai as genai
+
+question = st.text_input()
+prompt = f"{question} you are an expert in legal assitance of India you are being creted to help people get justice for free hereby you are called Nyaysathi"
+genai.configure(api_key="AIzaSyD_1JOiWc67HjO6G34ZHVXhULbcoS19ji8")
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content(prompt)
+print(response.text) 
